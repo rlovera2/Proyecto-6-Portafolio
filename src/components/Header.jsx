b9db62548c6175b7b08d03da819d57685f2a5772
Roberto Lovera <rlovera1cursos@gmail.com>
2024-01-24 10:38:18 -0500

    se actualizo header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,22 @@
 import PropTypes from "prop-types";
 import fondo1 from "../img/fondo1.jpg";
 import linked_azul1 from "../img/linkedin-azul1.png";
-//import linked_azul2 from "../img/linkedin-azul2.png";
+import linked_azul2 from "../img/linkedin-azul2.png";
 import indeed_azul1 from "../img/indeed-azul1.png";
+import indeed_azul2 from "../img/indeed-azul2.png";
 import github_azul1 from "../img/github-azul1.png";
+import github_azul2 from "../img/github-azul2.png";
 
 const Header = ({ title }) => {
   //************CAMBIANDO IMAGEN PRINCIPAL CUANDO TIENE EL FOCO ***************/
-  // const cambiarImagenOn = (id, img) => {
-  //   document.getElementById(id).src = img;
-  // };
+  const cambiarImagenOn = (id, img) => {
+    document.getElementById(id).src = img;
+  };
 
   //************CAMBIANDO IMAGEN PRINCIPAL CUANDO PIERDE EL FOCO ***************/
-  // const cambiarImagenOff = (id, img) => {
-  //   document.getElementById(id).src = { img };
-  // };
+  const cambiarImagenOff = (id, img) => {
+    document.getElementById(id).src = img;
+  };
 
   return (
     <header className="row col contenedor_header">
@@ -31,8 +33,8 @@ const Header = ({ title }) => {
             src={linked_azul1}
             alt="Linked"
             id="img_Linked"
-            // onMouseOver={cambiarImagenOn("img_Linked", linked_azul2)}
-            // onMouseOut={cambiarImagenOff("img_Linked", linked_azul1)}
+            onMouseOver={() => cambiarImagenOn("img_Linked", linked_azul2)}
+            onMouseOut={() => cambiarImagenOff("img_Linked", linked_azul1)}
           />
         </a>{" "}
         <a
@@ -40,14 +42,26 @@ const Header = ({ title }) => {
           target="_blank"
           rel="noreferrer"
         >
-          <img src={indeed_azul1} />
+          <img
+            src={indeed_azul1}
+            alt="Indeed"
+            id="img_Indeed"
+            onMouseOver={() => cambiarImagenOn("img_Indeed", indeed_azul2)}
+            onMouseOut={() => cambiarImagenOff("img_Indeed", indeed_azul1)}
+          />
         </a>{" "}
         <a
           href="https://github.com/rlovera2?tab=repositories"
           target="_blank"
           rel="noreferrer"
         >
-          <img src={github_azul1} />
+          <img
+            src={github_azul1}
+            alt="Github"
+            id="img_Github"
+            onMouseOver={() => cambiarImagenOn("img_Github", github_azul2)}
+            onMouseOut={() => cambiarImagenOff("img_Github", github_azul1)}
+          />
         </a>
         <br />
         <br />
